c5a20253970ac761c185608701a89f79bb4462ad
dsrebrak <serebryakov.d.1989@gmail.com>
2019-01-24 18:00:22 +0100

    stopped at 5_1

--- exercises/05_basic_scripts/task_5_1.py
+++ exercises/05_basic_scripts/task_5_1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
 Задание 5.1
@@ -18,4 +19,39 @@ Mask:
 Проверить работу скрипта на разных комбинациях сеть/маска.
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+
+#ip_cidr=input('IP address x.x.x.x/y:')
+ip_cidr='10.20.34.15/28'
+
+ip_s=ip_cidr.split('/')[0]
+mask=int(ip_cidr.split('/')[1])
+
+ip_int=[int(ip) for ip in ip_s.split('.')]
+mask_bin_s=mask*'1'+(32-mask)*'0'
+print(mask_bin_s)
+mask_s_l=[mask_bin_s[8*n:8*n+8] for n in range(4)]
+
+print(mask_s_l)
+#define template with argument numbers 0-3
+ip_template='''
+IP address:
+{0:<8} {1:<8} {2:<8} {3:<8} 
+{0:08b} {1:08b} {2:08b} {3:08b} 
+'''
+
+
+'''
+ip_l=IP.split('.')
+#make list integer
+ip_int=[int(ip) for ip in ip_l]
+#define template with argument numbers 0-3
+ip_template='''
+#IP address:
+#{0:<8} {1:<8} {2:<8} {3:<8} 
+#{0:08b} {1:08b} {2:08b} {3:08b} 
+'''
+print(ip_int)
+
+print(ip_template.format(ip_int[0],ip_int[1],ip_int[2],ip_int[3]))
 '''
