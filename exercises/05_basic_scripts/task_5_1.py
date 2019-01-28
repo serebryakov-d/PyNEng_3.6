9384e2cfbd3ea34a069a8cff3fa14c8d090a3b95
dsrebrak <serebryakov.d.1989@gmail.com>
2019-01-28 17:24:47 +0100

    task_5_1a in progress

--- exercises/05_basic_scripts/task_5_1.py
+++ exercises/05_basic_scripts/task_5_1.py
@@ -21,37 +21,30 @@ Mask:
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
-#ip_cidr=input('IP address x.x.x.x/y:')
-ip_cidr='10.20.34.15/28'
+ip_cidr=input('IP address x.x.x.x/y:')
+#ip_cidr='10.20.34.15/28'
 
 ip_s=ip_cidr.split('/')[0]
 mask=int(ip_cidr.split('/')[1])
 
 ip_int=[int(ip) for ip in ip_s.split('.')]
 mask_bin_s=mask*'1'+(32-mask)*'0'
-print(mask_bin_s)
 mask_s_l=[mask_bin_s[8*n:8*n+8] for n in range(4)]
+mask_n_l=[int(S,2) for S in mask_s_l]
 
-print(mask_s_l)
 #define template with argument numbers 0-3
-ip_template='''
-IP address:
+nw_template='''
+Network:
 {0:<8} {1:<8} {2:<8} {3:<8} 
 {0:08b} {1:08b} {2:08b} {3:08b} 
 '''
 
+mask_template='''
+Mask:
+{:<8} {:<8} {:<8} {:<8} 
+{:<8} {:<8} {:<8} {:<8} 
+'''
+print(nw_template.format(ip_int[0],ip_int[1],ip_int[2],ip_int[3]))
+#print(nw_template.format(*ip_int))
+print(mask_template.format(*mask_n_l,*mask_s_l))
 
-'''
-ip_l=IP.split('.')
-#make list integer
-ip_int=[int(ip) for ip in ip_l]
-#define template with argument numbers 0-3
-ip_template='''
-#IP address:
-#{0:<8} {1:<8} {2:<8} {3:<8} 
-#{0:08b} {1:08b} {2:08b} {3:08b} 
-'''
-print(ip_int)
-
-print(ip_template.format(ip_int[0],ip_int[1],ip_int[2],ip_int[3]))
-'''
--- exercises/05_basic_scripts/task_5_1a.py
+++ exercises/05_basic_scripts/task_5_1a.py
@@ -28,5 +28,38 @@ Mask:
 Проверить работу скрипта на разных комбинациях сеть/маска.
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
-
 '''
+
+#ip_cidr=input('IP address x.x.x.x/y:')
+ip_cidr='10.20.34.25/28'
+
+ip_s=ip_cidr.split('/')[0]
+mask=int(ip_cidr.split('/')[1])
+
+ip_int=[int(ip) for ip in ip_s.split('.')]
+print(ip_int)
+ip_bin_s=''.join(['{:08b}'.format(A) for A in ip_int])
+print(ip_bin_s)
+
+mask_bin_s=mask*'1'+(32-mask)*'0'
+mask_s_l=[mask_bin_s[8*n:8*n+8] for n in range(4)]
+mask_n_l=[int(S,2) for S in mask_s_l]
+
+nw=[(ip_bin_s[n] AND mask_bin_s[n]) for n in range(31)]
+print(nw)
+
+#define template with argument numbers 0-3
+nw_template='''
+Network:
+{0:<8} {1:<8} {2:<8} {3:<8} 
+{0:08b} {1:08b} {2:08b} {3:08b} 
+'''
+
+mask_template='''
+Mask:
+{:<8} {:<8} {:<8} {:<8} 
+{:<8} {:<8} {:<8} {:<8} 
+'''
+print(nw_template.format(ip_int[0],ip_int[1],ip_int[2],ip_int[3]))
+#print(nw_template.format(*ip_int))
+print(mask_template.format(*mask_n_l,*mask_s_l))
