40a3a486ed1adcbb40d1d62e921625af7c7503f8
dsrebrak <serebryakov.d.1989@gmail.com>
2019-02-01 15:45:15 +0100

    started 6.1a (should use try..except)

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
 Задание 6.1
@@ -19,3 +20,20 @@ D: 224-239
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+ip_addr=input('Enter dotted decimap IP address:')
+#ip_addr='365.11.11.11'
+
+ip1=int(ip_addr.split('.')[0])
+if ip1 >= 1 and ip1 <= 223:
+    result='unicast'
+elif ip1 >= 224 and ip1 <= 239:
+    result='multicast'
+elif ip_addr=='255.255.255.255':
+    result='local broadcast'
+elif ip_addr=='0.0.0.0':
+    result='unassigned'
+else:
+    result='unused'
+
+print(result)
--- exercises/06_control_structures/task_6_1a.py
+++ exercises/06_control_structures/task_6_1a.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
 Задание 6.1a
@@ -16,3 +17,23 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+#ip_addr=input('Enter dotted decimap IP address:')
+ip_addr='365.11.11.11'
+ip_l=ip_addr.split('.')
+dotted=False
+if 
+
+ip1=int(ip_addr.split('.')[0])
+if ip1 >= 1 and ip1 <= 223:
+    result='unicast'
+elif ip1 >= 224 and ip1 <= 239:
+    result='multicast'
+elif ip_addr=='255.255.255.255':
+    result='local broadcast'
+elif ip_addr=='0.0.0.0':
+    result='unassigned'
+else:
+    result='unused'
+
+print(result)
