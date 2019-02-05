7c85fc186e07a71b8a9edbdd1d945d2aee36a121
dsrebrak <serebryakov.d.1989@gmail.com>
2019-02-05 17:05:43 +0100

    6_2 done

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
 Задание 6.2
@@ -14,3 +15,7 @@
 mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
 
 mac_cisco = []
+for addr in mac:
+    mac_cisco.append(addr.replace(':','.'))
+
+print(mac_cisco)
